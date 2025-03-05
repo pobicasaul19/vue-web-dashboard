ca27b52c7fed93c441150724f21d0d98ff4e41e5
Pobi Casaul <phoebecasaul@gmail.com>
2025-03-05 11:00:06 +0800

    update auth controller

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -7,17 +7,15 @@ import { logger } from '../utils/index.js';
 import { mergeRequestData } from '../utils/mergeRequestData.js';
 
 // Generate access token
-// const generateAccessToken = (uuid) => {
-//   return jwt.sign({ uuid }, process.env.APP_TOKEN_KEY, { expiresIn: '7d' });
-// };
+const generateAccessToken = (uuid) => {
+  return jwt.sign({ uuid }, process.env.APP_TOKEN_KEY, { expiresIn: '7d' });
+};
 
 // Login user
 export const login = async (req, res) => {
   try {
     const usersCollection = await loadUserCollection();
     const { email, password } = mergeRequestData(req);
-
-    logger.info('Login request received', { body: req.body, query: req.query });
 
     const field = { email, password };
     const context = { usersCollection, email };
@@ -31,22 +29,12 @@ export const login = async (req, res) => {
       });
     }
 
-    const user = usersCollection.data.users.find(user => user.email === email);
-    if (!user) {
-      logger.warn('User not found', { email });
-      return res.status(404).json({
-        metadata: {
-          message: 'User not found'
-        }
-      });
-    }
-
-    // const accessToken = generateAccessToken(user.uuid);
+    const accessToken = generateAccessToken(user.uuid);
     const { password: _, ...userWithoutPassword } = user;
     logger.info('User authenticated successfully', { user: userWithoutPassword });
 
     res.status(200).json({
-      data: { user: userWithoutPassword, token: 'access-token-123' },
+      data: { user: userWithoutPassword, token: accessToken },
       metadata: { message: 'Authorized' },
     });
   } catch (error) {
